375a5201a87dc0d111d6ea2d6854cefdec4316bc
mdm06 <marjose123abc@gmail.com>
2024-01-24 09:39:32 +0100

    Creo variables y método main

---
+++ src/TresEnRayas.java
@@ -0,0 +1,15 @@
+ import java.util.Scanner;
+public class TresEnRayas {
+    private static char tablero [] = new char[9];
+    private static int victorias=0, derrotas=0;
+    private static final char charUsuario='x', charMaquina='0';
+
+     public static void main(String[] args) {
+         Scanner input=new Scanner (System.in);
+         int opcion=0;
+         do {
+             System.out.println();
+         }
+
+    }
+}
